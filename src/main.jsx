74f89bee011c21ea4bf73473fbf9b1c6842890b9
VladimirOnly <plokhotnuik@gmail.com>
2025-02-27 14:25:21 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,38 +7,6 @@ root.render(<App />);
 
 
 
-document.getElementById("contactForm").addEventListener("submit", async function(event) {
-    event.preventDefault();
-
-    const formData = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    };
-
-    const responseMessage = document.getElementById("formResponse");
-
-    try {
-        const response = await fetch("/api/send", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(formData)
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            responseMessage.textContent = "Děkujeme! Vaše zpráva byla odeslána.";
-            responseMessage.style.color = "green";
-            this.reset();
-        } else {
-            throw new Error(result.error || "Chyba při odesílání zprávy.");
-        }
-    } catch (error) {
-        responseMessage.textContent = error.message;
-        responseMessage.style.color = "red";
-    }
-});
-
 const sheetUrl = 'https://docs.google.com/spreadsheets/d/1Y423RJnyWCGu2UFceA5TWej1FufDx8lA/gviz/tq?tqx=out:json';
 
 async function loadProducts() {
@@ -99,3 +67,4 @@ document.getElementById('contactForm').addEventListener('submit', async function
         document.getElementById('formResponse').innerText = 'Chyba při odesílání zprávy!';
     }
 });
+
